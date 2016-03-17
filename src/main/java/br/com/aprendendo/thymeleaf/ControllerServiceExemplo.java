dfcc9a0d888bbecdad10bd97f102ac2a2962d014
Artur Drummond <arturbdr@gmail.com>
2016-03-17 13:22:11 -0300

    removido imports desnecessarios

--- src/main/java/br/com/aprendendo/thymeleaf/ControllerServiceExemplo.java
+++ src/main/java/br/com/aprendendo/thymeleaf/ControllerServiceExemplo.java
@@ -6,7 +6,6 @@ import javax.annotation.PostConstruct;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.domain.vo.PessoaVO;
